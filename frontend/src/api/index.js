fedb39eca6bb60b3def61fbeca828c828feac587
Joca0 <137853850+Joca0@users.noreply.github.com>
2026-01-06 17:42:35 -0500

    .

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://dumply-po26.onrender.com',
+  //baseURL: 'http://localhost:8080',
 });
 
 export const getEquipments = () => api.get('/equipments');
